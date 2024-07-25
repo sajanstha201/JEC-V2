0ae6318d9c55f1eca6867aff82e03f4e61694449
sabin gautam <sabin147369@gmail.com>
2024-07-25 16:53:04 +0545

    conflict resolve

--- src/App.js
+++ src/App.js
@@ -1,44 +1,29 @@
-
-
-import { Home } from './webpage/Home';
-import { HashRouter ,Routes,Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Footer from './components/Navbar/Footer';
 import Events from './components/AboutUsPage/Events';
-import AboutUniversity from './components/AboutUsPage/AboutUniversity';
 
-=======
-import './App.css';
-import { Home } from './webpage/Home';
-import { HashRouter ,Routes,Route } from 'react-router-dom';
+import About from './components/About/About';
+
+
+
 import { Header } from './components/Navbar/Header';
-import { AboutUsPage } from './components/AboutUsPage/AboutUsPage';
+import Home from './webpage/Home';
 
 function App() {
   return (
-    <div className="App">
-
-     
+    <HashRouter>
+      <div className="App">
+        <Header />
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
-         
-
-        <Header/>
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/admission'/>
-          <Route path='/about-us' element={<AboutUsPage/>}/>
-          <Route path='/academic'/>
-          <Route path='/facilities'/>
-          <Route path='/news'/>
-          <Route path='/contact-us'/>
+        <Route path="/" element={<Home />} />
+         <Route path="/about" element={<About />} />
 
         </Routes>
-        <Events/>
-        <AboutUniversity/>
-        <Footer/>
-      </HashRouter>
-    </div>
+      
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }
 
--- src/webpage/IntroductionSection.js
+++ src/webpage/IntroductionSection.js
@@ -1,8 +1,39 @@
-
-export const IntroductionSection = ()=>{
-    return(
-        <>
-        
-        </>
-    )
-}
+import styled from "styled-components";
+import jecimg from '../media/images/jec.png'
+const BackDivImg = styled.div`
+  background-image: url(${jecimg});
+  background-size: cover;
+  background-position: center;
+  height: 80vh;
+  width: 100%;
+  opacity: 1;
+`;
+export const IntroductionSection = () => {
+  return (
+    <>
+      <header>
+        {/* <video
+            className="w-full h-[95vh] object-cover -z-10 bgcolor relative"
+            autoPlay
+            muted
+          >
+            <source src={jecvideo} type="video/mp4" />
+          </video> */}
+        <BackDivImg className="sm:h-[70vh] w-full relative">
+          <div className="block justify-center w-full mx-auto md:w-[60%] p-4 text-center items-center text-white">
+            <div className="mt-40">
+              <h1 className="text-2xl font-bona-nova font-semibold ">JANAKPUR ENGINEERING COLLEGE</h1>
+              <p className="text-lg ">
+                Janakpur engineering is a leading research university pushing
+                boundaries and frontiers to brighten the future of the students.
+              </p>
+              <button className="px-4 py-3 font-semibold hover:bg-white hover:text-black mt-2 rounded-lg border border-white">
+                CONTACT US
+              </button>
+            </div>
+          </div>
+        </BackDivImg>
+      </header>
+    </>
+  );
+};
